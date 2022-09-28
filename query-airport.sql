9070431a04242ed9d43444a534bab9b8c9abc606
Cix8 <francesco.natal_fn@libero.it>
2022-09-28 17:44:38 +0200

    add query con group by in query-airport

--- query-airport.sql
+++ query-airport.sql
@@ -1,4 +1,41 @@
-﻿-- ++++++ QUERY SU SINGOLA TABELLA ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+﻿-- ++++++ QUERY CON GROUP BY ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+-- 6- Contare quanti voli ci sono stati ogni anno (tenendo conto della data di partenza) (11)
+SELECT COUNT(flights.id) AS flights_counter, YEAR(flights.departure_datetime) AS year
+FROM flights
+GROUP BY YEAR(flights.departure_datetime);
+
+-- 5- Contare quanti passeggeri sono nati nello stesso anno (61)
+SELECT COUNT(passengers.id) AS passengers_counter, YEAR(passengers.date_of_birth) AS year
+FROM passengers
+GROUP BY YEAR(passengers.date_of_birth);
+
+-- 4- Ordinare gli aerei per numero di manutenzioni ricevute (da quello che ne ha di piu'; dell'aereo vogliamo solo l'ID) (100)
+SELECT COUNT(maintenance_works.id) AS works_counter, maintenance_works.airplane_id
+FROM maintenance_works
+GROUP BY maintenance_works.airplane_id
+ORDER BY works_counter DESC;
+
+-- 3- Contare per ogni volo il numero di passeggeri (del volo vogliamo solo l'ID) (1000)
+SELECT COUNT(flight_passenger.id) AS passengers_counter, flight_passenger.flight_id
+FROM flight_passenger
+GROUP BY flight_passenger.flight_id;
+
+-- 2- Contare quante volte ogni impiegato ha lasciato una compagnia aerea (non mostrare quelli che non hanno mai lasciato; dell'impiegato vogliamo solo l'ID) (8939)
+SELECT COUNT(airline_employee.layoff_date), airline_employee.employee_id
+FROM airline_employee
+WHERE airline_employee.layoff_date IS NOT NULL
+GROUP BY airline_employee.employee_id;
+
+-- 1- Contare quanti lavori di manutenzione ha eseguito ogni impiegato (dell'impiegato vogliamo solo l'ID) (1136)
+SELECT COUNT(employee_maintenance_work.id) AS maintenance_work_counter, employee_maintenance_work.employee_id AS employee_id 
+FROM employee_maintenance_work
+GROUP BY employee_maintenance_work.employee_id;
+
+-- ++++++ QUERY CON GROUP BY ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+-- ++++++ QUERY SU SINGOLA TABELLA ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 -- *** BONUS ***********
 
