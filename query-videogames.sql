ab98ba5a4cda5fa7dff7a3d12c5d34231b7c01eb
Cix8 <francesco.natal_fn@libero.it>
2022-09-28 14:43:29 +0200

    add query con gruop by

--- query-videogames.sql
+++ query-videogames.sql
@@ -1,4 +1,41 @@
-﻿-- ++++++QUERY SU SINGOLA TABELLA++++++
+﻿-- ++++++QUERY CON GROUP BY++++++++++++
+
+
+-- 6- Ordinare i videogame in base alla media delle recensioni (del videogioco vogliamo solo l'ID) (500)
+SELECT AVG(rating) AS avg_rating, videogame_id
+FROM reviews
+GROUP BY videogame_id;
+
+-- 5- Contare quanti videogiochi sono disponbiili per ciascun device (del device vogliamo solo l'ID) (7)
+SELECT COUNT(videogame_id) AS videogame_counter, device_id
+FROM device_videogame
+GROUP BY device_id;
+
+-- 4- Mostrare il numero di videogiochi rilasciati ogni anno (11)
+SELECT COUNT(id) AS videogame_counter, YEAR(release_date) AS release_year
+FROM videogames
+GROUP BY YEAR(release_date);
+
+-- 3- Contare quanti videogiochi hanno ciascuna classificazione PEGI (della classificazione PEGI vogliamo solo l'ID) (13)
+SELECT COUNT(videogame_id) AS videogame_counter, pegi_label_id 
+FROM pegi_label_videogame
+GROUP BY pegi_label_id;
+
+-- 2- Contare quante recensioni ha ricevuto ogni videogioco (del videogioco vogliamo solo l'ID) (500)
+SELECT COUNT(id) AS reviews_counter, videogame_id
+FROM reviews
+GROUP BY videogame_id;
+
+-- 1- Contare quante software house ci sono per ogni paese (3)
+SELECT COUNT(id) AS software_house_counter, country
+FROM software_houses
+GROUP BY country;
+
+
+-- ++++++QUERY CON GROUP BY++++++++++++
+
+
+-- ++++++QUERY SU SINGOLA TABELLA++++++++++++
 
 
 -- ***BONUS***
@@ -69,4 +106,4 @@ FROM software_houses
 WHERE country = 'united states';
 
 
--- ++++++QUERY SU SINGOLA TABELLA++++++
+-- ++++++QUERY SU SINGOLA TABELLA++++++++++++
